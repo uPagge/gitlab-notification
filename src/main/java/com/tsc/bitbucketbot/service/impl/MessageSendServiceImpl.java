e2e56bebcfce5d5a2b02b3a17c3fd765c82ecccf
upagge <upagge@icloud.com>
2020-03-02 12:51:37 +0300

    Исправил вид ссылки при отправлении

--- src/main/java/com/tsc/bitbucketbot/service/impl/MessageSendServiceImpl.java
+++ src/main/java/com/tsc/bitbucketbot/service/impl/MessageSendServiceImpl.java
@@ -18,7 +18,7 @@ public class MessageSendServiceImpl implements MessageSendService {
     @Override
     public void add(@NonNull MessageSend messageSend) {
         messageSend.setMessage(
-                messageSend.getMessage().replace("localhost", "192.168.236.164:7990")
+                messageSend.getMessage().replace("localhost", "192.168.236.164")
         );
         messageSendRepository.add(messageSend);
     }
