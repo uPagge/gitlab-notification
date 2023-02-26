35fa39720cff9e9763f624d17bef004cf14e567a
Struchkov Mark <mark@struchkov.dev>
2023-02-26 10:26:53 +0300

    Добавил уведомление о сборке

--- ci-notify.sh
+++ ci-notify.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-TIME="10"
-URL="https://api.telegram.org/bot$DEPLOY_TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A-- -- -- -- --%0ABranch:+$CI_COMMIT_REF_SLUG%0AProject:+$CI_PROJECT_TITLE"
+if [ "$DRONE_BUILD_STATUS" = "success" ]; then
+  DEPLOY_STATUS="✅"
+else
+  DEPLOY_STATUS="❌"
+fi
 
-curl -s --max-time $TIME --proxy $PROXY_SETTING -d "chat_id=$TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT" $URL >/dev/null
+TIME="10"
+URL="https://api.telegram.org/bot$NOTIFY_TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $DEPLOY_STATUS%0A-- -- -- -- --%0ABranch:+$DRONE_TARGET_BRANCH%0ATag:+$DRONE_TAG%0AProject:+$DRONE_REPO_NAME"
+
+curl -s --max-time $TIME -d "chat_id=$NOTIFY_TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TEXT" $URL >/dev/null
